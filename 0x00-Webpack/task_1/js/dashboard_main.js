ce9201b65154e4354388efc2671ce3766180ea92
Linf0rd <linfordsnagger@gmx.com>
2024-04-02 17:03:07 +0200

    Create and configure Webpack for task_1 (package.json, scripts, webpack.config.js)

---
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,20 @@
+import $ from 'jquery';
+import _ from 'lodash';
+
+$(document).ready(function() {
+    $('body').append('<p>Holberton Dashboard</p>');
+    $('body').append('<p>Dashboard data for the students</p>');
+    $('body').append('<button id="startBtn">Click here to get started</button>');
+    $('body').append('<p id="count"></p>');
+    $('body').append('<p>Copyright - Holberton School</p>');
+
+    let count = 0;
+
+    function updateCounter() {
+        count++;
+        $('#count').text(`${count} clicks on the button`);
+    }
+
+    $('#startBtn').on('click', _.debounce(updateCounter, 300));
+});
+
