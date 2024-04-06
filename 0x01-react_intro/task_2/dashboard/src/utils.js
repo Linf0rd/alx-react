331d979af91de95ae39f8050a916244caed4ca9b
Linf0rd <linfordsnagger@gmx.com>
2024-04-06 07:49:07 +0200

    Notifications: enhance close button, list & priorities (task_2)
    utils.js: add getLatestNotification (task_2)

--- 0x01-react_intro/task_2/dashboard/src/utils.js
+++ 0x01-react_intro/task_2/dashboard/src/utils.js
@@ -5,3 +5,7 @@ export const getFullYear = () => {
 export const getFooterCopy = (isIndex) => {
   return isIndex ? "Holberton School" : "Holberton School main dashboard";
 };
+
+export function getLatestNotification() {
+  return "<strong>Urgent requirement</strong> - complete by EOD";
+}
