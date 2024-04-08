0837f117d43c4f9c86191ca9d7b20137f4fc529a
Linf0rd <linfordsnagger@gmx.com>
2024-04-08 05:56:21 +0200

    Babel setup: add presets & loader for jsx support (task_5)
    Webpack: configure babel-loader for src folder (task_5)
    
    * Babel:
        - Installed Babel and configured presets in .babelrc:
            - preset-env for modern JS compatibility.
            - preset-react for JSX transformation.
    * Webpack:
        - Added babel-loader to the configuration.
        - Configured babel-loader to handle js and jsx files in src folder.

---
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, '../dist'),
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, '../dist'),
+    },
+    port: 8080,
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './dist/index.html', // Specify the template file path
+    }),
+  ],
+};
