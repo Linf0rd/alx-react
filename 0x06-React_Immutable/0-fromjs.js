50311884cf44c04b6402979daf4039175489fd33
Linf0rd <linfordsnagger@gmx.com>
2024-04-26 17:09:44 +0200

    Setup project, install dependencies & create getImmutableObject function (alx-react)
    * Project Setup:
      * Copied necessary configuration files to the project root.
      * Executed npm install to install required dependencies.
    
    * 0-fromjs.js:
      * Introduced a new file named 0-fromjs.js.
      * Implemented a function named getImmutableObject that accepts an object as input.
      * The function utilizes Immutable.js library's fromJS function to convert the
        input object into an immutable Map object.

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -1,6 +1,7 @@
 const { fromJS } = require('immutable');
 
-export default function getImmutableObject(object) {
+function getImmutableObject(object) {
   return fromJS(object);
 }
+
 module.exports = getImmutableObject;
